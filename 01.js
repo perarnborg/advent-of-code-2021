e7e579884ccce8da23e4c642c6d7b4913a7d7efb
Per Arnborg <kontakt@perarnborg.se>
2021-12-03 00:09:50 +0100

    Day 1 star 2

--- 01.js
+++ 01.js
@@ -1,7 +1,7 @@
 const input = require('./inputs/01.json')
 
 console.log('Star 1')
-const count = input.reduce((data, number) => {
+const count1 = input.reduce((data, number) => {
     if (typeof data.previous !== 'undefined' && number > data.previous) {
         data.count++
     }
@@ -10,4 +10,15 @@ const count = input.reduce((data, number) => {
         previous: number
     }
 }, { count: 0 }).count
-console.log(count)
+console.log(count1)
+
+console.log('Star 2')
+const count2 = input.reduce((count, number, index) => {
+    if (index >= 3) {
+        if (input[index - 3] + input[index - 2] + input[index - 1] < input[index - 2] + input[index - 1] + input[index]) {
+            count++
+        }
+    }
+    return count
+}, 0)
+console.log(count2)
